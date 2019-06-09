3f14223afa7894f2e34edcfec8b1bcccff19314d
rustam <alidrake01@gmail.com>
2019-06-09 17:57:14 +0100

    bank fff

--- src/Accounting/Application.java
+++ src/Accounting/Application.java
@@ -8,7 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
         Bank bank = new Bank();
-        bank.accountName("HSBC");
+        bank.accountName("fff");
         bank.setBalance(100);
 //        LedgerAccount supplier = new LedgerAccount();
 //        LedgerAccount taxes = new LedgerAccount();
